eda7038bb2bd429fa163cd47eff06932b24d6ce0
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-12-04 17:19:38 -0300

    integration tests to find team by id

--- app/backend/src/tests/team.test.ts
+++ app/backend/src/tests/team.test.ts
@@ -13,16 +13,34 @@ const { app } = new App();
 
 const { expect } = chai;
 
-describe('Teams Test', function() {
+describe('Teams Integration Tests', function() {
     afterEach(function () {
         sinon.restore();
       });
-    it.only('Should return all teams', async function() {
+    it('Should return all teams', async function() {
         sinon.stub(SequelizeTeam, 'findAll').resolves(teams.allTeams as any);
         
         const { status, body } = await chai.request(app).get('/teams');
         
         expect(status).to.equal(200);
         expect(body).to.deep.equal(teams.allTeams);
+    });
+
+    it('Should return a team by id', async function() {
+        sinon.stub(SequelizeTeam, 'findByPk').resolves(teams.allTeams[0] as any);
+        
+        const { status, body } = await chai.request(app).get('/teams/1');
+        
+        expect(status).to.equal(200);
+        expect(body).to.deep.equal(teams.allTeams[0]);
+    });
+
+    it('Should return a error if the team doesn\'t exists', async function() {
+        sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+        
+        const { status, body } = await chai.request(app).get('/teams/9999');
+        
+        expect(status).to.equal(404);
+        expect(body.message).to.equal('Team not found');
     });
 })
