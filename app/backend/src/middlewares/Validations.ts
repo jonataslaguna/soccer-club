c6f49c760c2f68cd8fb7596892c1c1622cfe75c2
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-12-05 16:45:48 -0300

    fix Dockerfile and login validate middleware

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -7,11 +7,8 @@ class Validations {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
     const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-    if (!emailRegex.test(email)) {
-      return res.status(401).json({ message: 'Invalid email' });
-    }
-    if (password.length < 6) {
-      return res.status(401).json({ message: 'Invalid password' });
+    if (!emailRegex.test(email) || password.length < 6) {
+      return res.status(401).json({ message: 'Invalid email or password' });
     }
     next();
   }
