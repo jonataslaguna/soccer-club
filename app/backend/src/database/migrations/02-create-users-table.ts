6e2d9a2bc4f7d5b043d8f0eb0ab1ffb004837d6a
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-12-05 11:47:09 -0300

    implemented SequelizeMatch model and migration

--- app/backend/src/database/migrations/02-create-users-table.ts
+++ app/backend/src/database/migrations/02-create-users-table.ts
@@ -4,28 +4,28 @@ import IUser from '../../Interfaces/User/IUser';
 export default {
   up(queryInterface: QueryInterface) {
     return queryInterface.createTable<Model<IUser>>('users', {
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-        },
-        username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        },
-        role: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        },
-        email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        },
-        password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-       },
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+      },
+      username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+      },
+      role: {
+            type: DataTypes.STRING,
+            allowNull: false,
+      },
+      email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+      },
+      password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+      },
     });
   },
 
